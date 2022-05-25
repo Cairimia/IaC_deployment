c9c750b892bc2acb7e2466573177170caf6d7155
catalini <catalini@ext.inditex.com>
2022-05-26 02:26:54 +0300

    iac

--- IaC/terraform/storage_account.tf
+++ IaC/terraform/storage_account.tf
@@ -6,3 +6,12 @@ resource "azurerm_storage_account" "sa_IaC" {
   account_tier             = "Standard"
   account_replication_type = var.function_storage_account_replication_type
 }
+
+#### Storage Account for all functions ####
+resource "azurerm_storage_account" "sa_IaC_test" {
+  name                     = "${var.project}sta${var.location.code}${var.environment}IAC"
+  location                 = var.location.name
+  resource_group_name      = var.resource_group_name
+  account_tier             = "Standard"
+  account_replication_type = var.function_storage_account_replication_type
+}
