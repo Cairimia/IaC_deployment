c6bccae81c98f240e0f1709427e1532d5c4bf023
catalini <catalini@ext.inditex.com>
2022-05-26 00:52:40 +0300

    IAC

--- IaC/terraform/environments/pro/main.pro.tf
+++ IaC/terraform/environments/pro/main.pro.tf
@@ -45,3 +45,80 @@ provider "azurerm" {
     }
   }
 }
+
+# Provider Variables
+variable "azurerm_subscription_id" {
+  type = string
+}
+variable "azurerm_client_id" {
+  type = string
+}
+variable "azurerm_client_secret" {
+  type = string
+}
+variable "azurerm_tenant_id" {
+  type = string
+}
+
+# Project Variables
+variable "business_unit" {
+  type = string
+}
+variable "department" {
+  type = string
+}
+
+variable "location" {
+  type = map(string)
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "unique_string" {
+  type = string
+}
+variable "resource_group_name" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "project_iteration" {
+  type = number
+}
+
+variable "binding_tags" {
+  type    = map(string)
+  default = {}
+
+  description = "Binding tags defined by resource"
+}
+
+locals {
+  project_tags = merge(
+    var.binding_tags,
+    {
+      "environment" = var.environment
+    }
+  )
+}
+
+variable "keyvault_name" {
+  type = string
+}
+
+variable "group_object_id" {
+  type = string
+}
+
+variable "keyvault_unique_string" {
+  type = string
+}
+
+variable "function_storage_account_replication_type" {
+  type = string
+}
