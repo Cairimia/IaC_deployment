3dd60e43ad16f464ff6d6d65a503eb0a7d0ade15
catalini <catalini@ext.inditex.com>
2022-06-03 03:19:06 +0300

    iac

--- IaC/terraform/main.tf
+++ IaC/terraform/main.tf
@@ -9,6 +9,11 @@ terraform {
   }
 }
 provider "azurerm" {
+  subscription_id = var.azurerm_subscription_id
+  client_id       = var.azurerm_client_id
+  client_secret   = var.azurerm_client_secret
+  tenant_id       = var.azurerm_tenant_id
+  
   features {
     key_vault {
       recover_soft_deleted_key_vaults = true
