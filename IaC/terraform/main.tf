25a71b081edcd0d1fdcfc087e2b55c3d0e42f3fa
catalini <catalini@ext.inditex.com>
2022-06-03 04:01:36 +0300

    iac

--- IaC/terraform/main.tf
+++ IaC/terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = "~> 1.2.0"
   backend "azurerm" {}
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.7.0"
+      version = "~> 3.8.0"
     }
   }
 }
