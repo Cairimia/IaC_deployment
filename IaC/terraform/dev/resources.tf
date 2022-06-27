123c7362d1874269a538541b5225d88a34e79357
catalini <catalini@ext.inditex.com>
2022-06-27 17:44:17 +0300

    iac

--- IaC/terraform/dev/resources.tf
+++ IaC/terraform/dev/resources.tf
@@ -22,3 +22,11 @@ resource "azurerm_storage_account" "mas" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_account" "mas" {
+  name                     = "testcata${var.environment_def}22"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
