fa8c1a71621542873a931f96f269611fafa0a260
Cairimia <78433327+Cairimia@users.noreply.github.com>
2022-06-28 17:33:41 +0300

    Feature/tests (#65)
    * tests
    
    * iac
    
    * tests
    
    * iac
    
    * ias
    
    * iac
    
    * iac
    
    * iac

--- IaC/terraform/dev/resources.tf
+++ IaC/terraform/dev/resources.tf
@@ -15,16 +15,8 @@ resource "azurerm_storage_account" "mas" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_account" "mas_33" {
-  name                     = "testcata${var.environment_def}33"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_account" "mas_44" {
-  name                     = "testcata${var.environment_def}44"
+resource "azurerm_storage_account" "mas_66" {
+  name                     = "testcata${var.environment_def}66"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
